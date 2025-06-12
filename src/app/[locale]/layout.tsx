724a243e5b5248f5bf243053d2a072fba1f55667
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-06-12 11:31:49 +0000

    puedes poner de fondo algo como estrellas o cosas asi

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -82,7 +82,7 @@ export default function LocaleLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col bg-background text-foreground">
+      <body className="font-body antialiased min-h-screen flex flex-col text-foreground"> {/* Removed bg-background */}
         <SidebarProvider defaultOpen={true}> 
           {/* Pass only locale to LayoutContent; it will fetch its own dictionary */}
           <LayoutContent locale={currentLocale}>
