c78ba50a895b515ccd8ac8280c8258282cae3a7b
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-06-14 21:43:08 +0000

    el color de fondo esta como por debajo de la capa y no se ve

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -322,8 +322,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
-        // Desktop inset styling - mobile doesn't have the 'peer' structure in the same way
+        "relative flex min-h-svh flex-1 flex-col", // Removed bg-background
         "md:peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -767,5 +766,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
