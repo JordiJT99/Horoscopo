ed0b9414e142597476d7a22443df000d20c1ae37
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-07-04 16:59:11 +0000

    Pulling image: gcr.io/k8s-skaffold/pack Using default tag: latest latest

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -24,6 +23,12 @@ const nextConfig: NextConfig = {
   // The i18n object from next.config.js is for the Pages Router.
   // For App Router, i18n is typically handled by middleware and [locale] dynamic segments.
   // We will use middleware for routing.
+  webpack: (config) => {
+    config.externals.push({
+      handlebars: 'commonjs handlebars',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
