11e4d5232b668cfbb5aa353468f59c68dd3e99ca
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-07-04 06:53:57 +0000

    me sale este error al intentar cargar la carta natal desde el movil

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.astromistica.app',
+  appName: 'AstroMística : Tarot, Horóscopo y Carta Natal',
+  webDir: 'out'
+};
+
+export default config;
