c300af837de7b270f4526f404bae822c42c7bc6c
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-07-08 21:38:57 +0000

    output export no puedes utilzarlo porque si no, no puedo publicar

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.astromistica.app',
+  appName: 'AstroMística : Tarot, Horóscopo y Carta Natal',
+  webDir: 'public',
+  server: {
+    url: 'https://astromistica.org',
+    cleartext: true,
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#1A142B",
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "large",
+      spinnerColor: "#E11D48",
+      showSpinner: true
+    }
+  }
+};
+
+export default config;
