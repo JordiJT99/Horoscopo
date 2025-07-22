e6d9dfd4481b9be8375374dfedad4e027a4e9363
Jordi juann tur <jordi.jordi.jordi9@gmail.com>
2025-07-22 16:50:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,6 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Add this line
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
